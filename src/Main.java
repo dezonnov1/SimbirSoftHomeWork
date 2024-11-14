357f92226a652e70811e078b7400774bf08dc50d
Даниил Кузнецов <daniil73k@gmail.com>
2024-11-14 21:30:48 +0400

    1 задание есть!

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 /**
  * Класс Main, который использует функции классов в проекте для демонстрации выполненного задания.<br>
- * ✅ иррархия <b>weapon</b> -> <b>gun</b> -> <b>smg</b> получается 2 потомка<br>
- * ✅ 2 интерфейса и каждый из них реализован(из <b>shooting</b> в <b>gun</b>, а из <b>modificate</b> в <b>weapon</b>)<br>
- * ✅ абстрактный класс entity c абстрактной функцией printInfo()<br>
- * ✅ КАЖДЫЙ класс содержит >2 полей<br>
- * ✅ в каждом классе есть параметризированный конструктор<br>
- * ✅ реализация инкапсуляции в entity, класс coords, где сеттеры posX posY являются private<br>
- * ✅ статическое поле и метод в entity, counter и displayCounter()<br>
- * ✅ методы в каждом классе<br>
- * ✅ методы реализовывают какую-то сложную логику<br>
+ *
+ *  1. Добавить в классовую модель из домашнего задания 1 (или написать новую модель)
+ * примеры использования дженерик-классов (обобщенных типов) и дженерик-интерфейсов.<br>
+ *
+ * 2. Написать функциональный интерфейс с методом, который принимает число и возвращает
+ * булево значение. Написать реализацию такого интерфейса в виде лямбда-выражения, которое возвращает
+ * true если переданное число делится без остатка на 13.<br>
+ *
+ * 3. Написать функциональный интерфейс с методом, который принимает три дробных числа: a, b, c и
+ * возвращает тоже дробное число. Написать реализацию такого интерфейса в виде лямбда-выражения,
+ * которое возвращает дискриминант. Кто забыл, D = b^2 — 4ac.
  */
 public class Main {
     /**
@@ -16,51 +18,17 @@ public class Main {
      * @param args Не используется.
      */
     public static void main(String[] args) {
+        Table<String> stol1 = new Table<>("ТаблЭтка");
+        /*Воруем таблЭтку*/
+        System.out.println( stol1.stealItem() ); // ТаблЭтка
+        /*А сейчас ничего не получим*/
+        System.out.println( stol1.stealItem() ); // null
+
+        /*Тут такая же картина.*/
+        Table<Integer> stolInt = new Table<>(52);
+        System.out.println( stolInt.stealItem() ); // 52
+        System.out.println( stolInt.stealItem() ); // null
 
 
-        // у нас есть игрок и оружие
-        player Bob = new player("Bob");
-        Bob.printInfo();
-
-        player BigBob = new player("Big Bob", 200);
-        Bob.printInfo();
-        // printInfo и displayInfo это по сути выполняют одно и тоже, но printInfo это унаследованный от абстрактного класса
-        weapon axe = new weapon("axe", 19);
-        axe.displayInfo();
-
-        gun pistol = new gun("p250", 14,10);
-        pistol.displayInfo();
-
-        smg p90 = new smg("P90",27,50,"auto");
-        p90.displayInfo();
-
-        Bob.printInfo();
-        BigBob.printInfo();
-        System.out.println("Сущности созданы и получена информация о них\n");
-
-        Bob.attack(pistol, BigBob);
-        BigBob.printInfo();
-        System.out.println("О нееет! Bob стреляет в BigBob'a!\n");
-
-        Bob.attack(axe, BigBob);
-        BigBob.printInfo();
-        System.out.println("Ужас!!! Bob бьёт BigBob'a топором!\n");
-
-        if(!BigBob.getLive()){
-            BigBob.setHealthPoint(200);
-            BigBob.setLive(true);
-            BigBob.printInfo();
-            System.out.println("Возвращаем BigBob'a к жизни\n");
-        }
-        else {
-            BigBob.printInfo();
-            System.out.println();
-        }
-
-        p90.ChangeFireMode("semi");
-
-        Bob.attack(p90, BigBob);
-        BigBob.printInfo();
-        System.out.println("О нееет! Bob расстреливает BigBob'a!\n");
     }
 }
