8db805ce67564948d6360a4abe1dfd98e441b014
Даниил Кузнецов <daniil73k@gmail.com>
2024-11-14 23:28:00 +0400

    Выполнено

---
+++ src/Discriminant.java
@@ -0,0 +1,6 @@
+
+
+@FunctionalInterface
+public interface Discriminant<N extends Double> {
+    N sup(N a,N b,N c);
+}
--- src/Main.java
+++ src/Main.java
@@ -18,28 +18,38 @@ public class Main {
      * @param args Не используется.
      */
     public static void main(String[] args) {
+        // (1)
         Table<String> stol1 = new Table<>("ТаблЭтка");
         /*Воруем таблЭтку*/
-        System.out.println( stol1.stealItem() ); // ТаблЭтка
+        System.out.println( "Что своровали со стола stol1 " + stol1.stealItem() ); // ТаблЭтка
         /*А сейчас ничего не получим*/
-        System.out.println( stol1.stealItem() ); // null
+        System.out.println( "Что своровали со стола stol1 " + stol1.stealItem() ); // null
 
         /*Тут такая же картина.*/
         Table<Integer> stolInt = new Table<>(52);
-        System.out.println( stolInt.stealItem() ); // 52
-        System.out.println( stolInt.stealItem() ); // null
+        System.out.println( "Что своровали со стола stolInt " + stolInt.stealItem() ); // 52
+        System.out.println( "Что своровали со стола stolInt " + stolInt.stealItem() ); // null
 
-
+        // (2)
         int testI =24;
         double testD =26.0;
 
         DevisionOn<Integer> devisionOn13I = x-> (x % 13) == 0;
         System.out.println("Результат при " + testI + " % 26 = " + devisionOn13I.pred(testI));
 
-        DevisionOn<Double> devisionOn13D = x-> (-0.0001 < (x % 13)) && ((x % 13) < 0.0001); //
+        DevisionOn<Double> devisionOn13D = x-> (-0.0001 < (x % 13)) && ((x % 13) < 0.0001); // даем диапазон для принятия погрешности округления
         System.out.println("Результат при " + testD + " % 26 = " + devisionOn13D.pred(testD));
 
+        // (3)
+        double a = 1.0;
+        double b = 2.0;
+        double c = 1.0;
 
-
+        Discriminant<Double> discriminant = (la, lb, lc) -> (lb*lb-4*la*lc);
+        System.out.println(String.format(("a = %.02f, b = %.02f, c = %.02f, D = %.02f"),a,b,c,discriminant.sup(a,b,c)));
+        a = 2.4;
+        b = 7.2;
+        c = 3.2;
+        System.out.println(String.format(("a = %.02f, b = %.02f, c = %.02f, D = %.02f"),a,b,c,discriminant.sup(a,b,c)));
     }
 }
