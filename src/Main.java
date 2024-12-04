64d35f03be2dc42cfb9ed398bfd8d94ff272f6f2
Даниил Кузнецов <daniil73k@gmail.com>
2024-12-04 19:36:20 +0400

    4 задание есть!

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,32 @@
-/**
- * Класс Main, который использует функции классов в проекте для демонстрации выполненного задания.<br>
- *
- *  1. Добавить в классовую модель из домашнего задания 1 (или написать новую модель)
- * примеры использования дженерик-классов (обобщенных типов) и дженерик-интерфейсов.<br>
- *
- * 2. Написать функциональный интерфейс с методом, который принимает число и возвращает
- * булево значение. Написать реализацию такого интерфейса в виде лямбда-выражения, которое возвращает
- * true если переданное число делится без остатка на 13.<br>
- *
- * 3. Написать функциональный интерфейс с методом, который принимает три дробных числа: a, b, c и
- * возвращает тоже дробное число. Написать реализацию такого интерфейса в виде лямбда-выражения,
- * которое возвращает дискриминант. Кто забыл, D = b^2 — 4ac.
- */
+
+import java.util.random.RandomGenerator;
+
 public class Main {
     /**
      * Это основной метод, использующий методы классов.
      * @param args Не используется.
      */
     public static void main(String[] args) {
-        // (1)
-        Table<String> stol1 = new Table<>("ТаблЭтка");
-        /*Воруем таблЭтку*/
-        System.out.println( "Что своровали со стола stol1 " + stol1.stealItem() ); // ТаблЭтка
-        /*А сейчас ничего не получим*/
-        System.out.println( "Что своровали со стола stol1 " + stol1.stealItem() ); // null
+        /**
+         * Я как обычно лишний раз замарочился
+         * пополнение List'a со студентами пополсянется несколькими способами.
+         * Либо мы передаем параметры в функцию и внутри этой функции создается объект класса Students и помещается
+         * в List или мы передаем сам объект класса Students и внутри
+         */
+        DeanOffice dean = new DeanOffice();
+        String[] subjects = {"Русский", "Математика", "Информатика"};
+        dean.addStudentInStudentDataBase("Аня", "ИТСС", 1, subjects, new int[] {2,2,3});
+        dean.addStudentInStudentDataBase("Глеб", "ЖТО", 2, subjects, new int[] {3,3,3});
+        dean.addStudentInStudentDataBase("Боря", "ЖТО", 3, subjects, new int[] {4,4,3});
+        dean.addStudentInStudentDataBase("Ира", "ИТСС", 3, subjects, new int[] {5,5,5});
 
-        /*Тут такая же картина.*/
-        Table<Integer> stolInt = new Table<>(52);
-        System.out.println( "Что своровали со стола stolInt " + stolInt.stealItem() ); // 52
-        System.out.println( "Что своровали со стола stolInt " + stolInt.stealItem() ); // null
-
-        // (2)
-        int testI =24;
-        double testD =26.0;
-
-        DevisionOn<Integer> devisionOn13I = x-> (x % 13) == 0;
-        System.out.println("Результат при " + testI + " % 26 = " + devisionOn13I.pred(testI));
-
-        // делаем диапазон для из-за погрешности округления
-        DevisionOn<Double> devisionOn13D = x-> (-0.0001 < (x % 13)) && ((x % 13) < 0.0001);
-        System.out.println("Результат при " + testD + " % 26 = " + devisionOn13D.pred(testD));
-
-        // (3)
-        double a = 1.0;
-        double b = 2.0;
-        double c = 1.0;
-
-        Discriminant<Double> discriminant = (la, lb, lc) -> (lb*lb-4*la*lc);
-        System.out.println(String.format(("a = %.02f, b = %.02f, c = %.02f, D = %.02f"),a,b,c,discriminant.sup(a,b,c)));
-        a = 2.4;
-        b = 7.2;
-        c = 3.2;
-        System.out.println(String.format(("a = %.02f, b = %.02f, c = %.02f, D = %.02f"),a,b,c,discriminant.sup(a,b,c)));
+        dean.printAllStudents();
+        System.out.println("Студенты 3-го курса");
+        dean.printStudents(dean.getStudentDataBase(),3);
+        System.out.println("Удаляем " + dean.delBadStudents()+" студентов, со средним баллом <3.");
+        System.out.println("delStuds<3");
+        dean.printAllStudents();
+        System.out.println("Переводим на курс старше" + dean.transferToOlderCourse()+" студентов, со средним балом >=3");
+        dean.printAllStudents();
     }
 }
